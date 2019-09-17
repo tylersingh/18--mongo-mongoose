a67dd215d6f1aced51e4a3af16176e472019a57b
tylersingh <tylersingh@users.github.com>
2019-09-16 20:19:23 -0400

    updated server code

--- server.js
+++ server.js
@@ -1,42 +1,24 @@
-// Web Scraper Homework Solution Example
-// (be sure to watch the video to see
-// how to operate the site in the browser)
-// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const routes = require("./routes");
 
-// Require our dependencies
-var express = require("express");
-var mongoose = require("mongoose");
-var exphbs = require("express-handlebars");
-
-// Set up our port to be either the host's designated port, or 3000
-var PORT = process.env.PORT || 3000;
-
-// Instantiate our Express App
-var app = express();
-
-// Require our routes
-var routes = require("./routes");
-
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect Handlebars to our Express app
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Have every request go through our route middleware
 app.use(routes);
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
 
-// Listen on the port
 app.listen(PORT, function() {
   console.log("Listening on port: " + PORT);
 });
